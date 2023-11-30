0c2918ac5daf710889fb86b7e32a4b41deb5b1f9
BuildTools <krutikovasjustinas@gmail.com>
2023-11-30 10:56:59 +0200

    Trečios JS paskaitos medžiaga

--- 2023-11-29/js/pvz2.js
+++ 2023-11-29/js/pvz2.js
@@ -3,11 +3,10 @@
 //2. Jei password kintamasis yra 8 simbolių ar ilgesnis, į konsolę parašyti "Slaptažodis tinkamas"
 //3. Jei slaptažodis yra trumpesnis nei 8 simboliai, į konsolę parašyti "Slaptažodis per trumpas"
 
-let password = 'ledinukas';
+let password = "ledinukas";
 
-if(password.length > 8){
-    console.log("Slaptažodis tinkamas");
-}
-else{
-    console.log("Slaptažodis per trumpas");
+if (password.length > 8) {
+	console.log("Slaptažodis tinkamas");
+} else {
+	console.log("Slaptažodis per trumpas");
 }
