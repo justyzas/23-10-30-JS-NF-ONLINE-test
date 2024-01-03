642e855e486b3a58c71a8f764ed66877f335bc42
BuildTools <krutikovasjustinas@gmail.com>
2024-01-03 08:15:06 +0200

    pridedu urlus prie githubo

---
+++ 2024-01-03/script.js
@@ -0,0 +1,20 @@
+const allUrls1 = [
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka",
+	],
+	allUrls2 = [
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Scotch",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Triple_sec",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Applejack",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka",
+	],
+	allUrls3 = [
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Scotch",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Triple_sec",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Applejack",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Brandy",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Lemon_vodka",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Champagne",
+		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Sherry",
+	];
