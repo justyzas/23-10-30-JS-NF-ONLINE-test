3d81e871efe1c67586d1f8df3bffc09f60ebfaf0
BuildTools <krutikovasjustinas@gmail.com>
2024-01-24 10:42:29 +0200

    2024-01-24 paskaitos kodas + komentarai

--- 2024-01-24/app.js
+++ 2024-01-24/app.js
@@ -2,34 +2,41 @@ const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect(`mongodb+srv://Justelio19:xLHDNnNygV9woSRN@forum.jrfji5c.mongodb.net/forumhub`)
+//Prisijungimas prie duomenų bazės pasinaudojant URL
+mongoose.connect(`mongodb+srv://vartotojas:slaptazodis@forum.jrfji5c.mongodb.net/forumhub`);
 const db = mongoose.connection;
 
 //DB listeneriai, kurie nusako ar prie DB  buvo prisijungta sėkmingai ar ne
-db.on('error', (error)=>{
+db.on('error', (error)=>{//ERROR listeneris
 	console.error('erroras: ' + error);
 })
-db.once('open', ()=>{
+db.once('open', ()=>{//PRISIJUNGIMO listeneris
 	console.info('Prie duomenų bazės buvo sėkmingai prisijungta')
 })
 
 // Nustatymas EJS aktyvavimui
 app.set("view engine", "ejs");
 
+// Tarpinio routo sukūrimas
 const publicRouter = express.Router();
-publicRouter.use(express.static("public"));
-app.use("/public", publicRouter);
+//Statinių failų atvaizdavimas per /public aplanką
+publicRouter.use(express.static("public")); 
+//Tarpinio routo panaudojimas, pasiekiamas per http://localhost/public endpoint'ą
+app.use("/public", publicRouter); 
 
 app.get("/", (req, res) => {
+	//index.ejs failo atvaizdavimas iš views aplanko
 	res.render("index", {
 		title: "Forumo aplikacija",
 		username: "Justelio19",
 		list: ['Product1', 'Product2', 'Milk', 'Choclate']
 	});
+	//Kartu paduodami ir parametrai EJS failui
 });
 
 app.get('/register', (req,res)=>{
 	res.render('register');
+	//Register rout'as skirtas registracijai
 })
 
 app.listen(3000, () => {
